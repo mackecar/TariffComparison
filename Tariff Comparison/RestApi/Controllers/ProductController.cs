19997afa30d41dfd0bf50bb539c70caf5cf29885
dmacura <dmacura@smartcodehouse.com>
2020-03-18 10:04:48 +0100

    Added web api procject.

---
+++ Tariff Comparison/RestApi/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.ServiceIntefaces;
+using Domain.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IComparationService _comparationService;
+
+        public ProductController(IComparationService comparationService)
+        {
+            _comparationService = comparationService;
+        }
+
+        [HttpGet]
+        [Route("GetComparation")]
+        public IActionResult GetComparation()
+        {
+            try
+            {
+                List<ProductUsage> comparationResult = _comparationService.GetComparation(4500);
+
+                return Ok(comparationResult);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHelper.ErrorResponse(ex.Message));
+            }
+        }
+    }
+}
